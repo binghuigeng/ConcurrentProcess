40c7e1a2c0b1dd108fdfc7fcfd8d89e7c347b296
geng <geng@example.com>
2025-02-14 09:19:35 +0800

    模拟定时向线程池添加任务

--- main.cpp
+++ main.cpp
@@ -17,8 +17,6 @@ int process(std::vector<T> *aSrc)
 template<typename T>
 bool process2(T *aSrc)
 {
-    // 模拟耗时操作
-    std::this_thread::sleep_for(std::chrono::seconds(1)); // 模拟耗时
     *aSrc *= 10; // 简单地将数据乘以 2
 
     return true;
@@ -33,40 +31,26 @@ int main()
     ThreadPool pool(4);
 
     // 模拟输入数据流
-    std::vector<std::vector<int>> inputBuffers;
-    for (int i = 0; i < 10; ++i) {
-        inputBuffers.emplace_back(10, i); // 每个 vector 包含 10 个相同的数
-    }
+    for (int i = 0; i < 16; ++i) {
+        std::vector<int> buffer(10, i);
 
-    // 提交任务并收集结果
-    std::vector<std::future<int>> results;
-    for (auto& buffer : inputBuffers) {
-        results.push_back(pool.enqueue(process<int>, &buffer)); // 提交任务
-    }
+        // enqueue and store future
+        auto result = pool.enqueue(process<int>, &buffer);
 
-    double dt = 999.25;
+        auto ret = result.get();
+        std::cout << "ret " << ret << std::endl;
 
-    // enqueue and store future
-    auto result = pool.enqueue(process2<double>, &dt);
-
-    // 等待所有任务完成并输出结果
-    for(auto && result: results)
-        std::cout << result.get() << ' ';
-    std::cout << std::endl;
-
-    auto ret = result.get();
-    std::cout << "ret " << ret << std::endl;
-
-    // 输出修改后的 inputBuffers
-    for (size_t i = 0; i < inputBuffers.size(); ++i) {
         std::cout << "Buffer " << i << ": ";
-        for (int val : inputBuffers[i]) {
-            std::cout << val << " ";
+        for (auto && val : buffer) {
+            std::cout << val << ' ';
         }
         std::cout << std::endl;
-    }
 
-    std::cout << dt << std::endl;
+        auto ret2 = process2(&buffer[0]);
+        std::cout << "ret2 " << ret2 << ' ' << buffer[0] << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(250)); // 模拟耗时
+    }
 
     // 获取当前时间，作为开始时间
     auto end = std::chrono::high_resolution_clock::now();
