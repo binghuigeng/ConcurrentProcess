cb65338c133f3e3fcd9e57f9991492d8239fe0a5
geng <geng@example.com>
2025-02-18 18:36:00 +0800

    修改错误注释

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main()
         std::cout << "added to the pool tasks: " << counter << std::endl;
     }
 
-    // 获取当前时间，作为开始时间
+    // 获取当前时间，作为结束时间
     auto end = std::chrono::high_resolution_clock::now();
 
     // 计算时间差，单位是微妙
